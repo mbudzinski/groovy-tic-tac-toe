428d9516a12022c416e29bfb6bc0e89ba2c08eeb
marcinbudzinski <marcin.budzinski@hrs.com>
2017-10-02 16:52:01 +0200

    Made BoardDrawer class more readable

--- src/main/groovy/pl/mbudzinski/tictactoe/board/BoardDrawer.groovy
+++ src/main/groovy/pl/mbudzinski/tictactoe/board/BoardDrawer.groovy
@@ -3,21 +3,34 @@ package pl.mbudzinski.tictactoe.board
 
 class BoardDrawer {
 
+    private static final String WHITESPACE = ' '
+    private static final String ROW_SEPARATOR = '--'
+
     void printBoard(board) {
+        printHeader(board)
+        printRows(board)
+    }
+
+    private void printHeader(board) {
         board.eachWithIndex { row, index ->
-            print(index + ' ')
+            print(index + WHITESPACE)
         }
-        println ' '
+        println WHITESPACE
+    }
+
+    private void printRows(board) {
         board.eachWithIndex { row, index ->
-            print row.join('|')
-            print(' ' + index)
-            println ' '
-            if (index < board.size() - 1) {
-                row.each({ column -> print('--') })
-                println ' '
-            }
+            printRow(board, row, index)
         }
     }
 
-
+    private void printRow(board, row, index) {
+        print row.join('|')
+        print(WHITESPACE + index)
+        println WHITESPACE
+        if (index < board.size() - 1) {
+            row.each({ column -> print(ROW_SEPARATOR) })
+            println WHITESPACE
+        }
+    }
 }
